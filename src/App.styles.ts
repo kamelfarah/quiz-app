adc485810521ae19deee478657b6c0caf7e0a78a
tiab kamel farah <taibkamel13@gmail.com>
2023-02-27 20:20:06 +0100

    feat: delete not used variables

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,8 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components";
 
-import BackGroundImg from './images/background.jpg'
-
-
 export const GlbStyle = createGlobalStyle`
  html {
     height :100%;
